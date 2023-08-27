1dee971477080b37df4c773814b3b477d00b875d
Max Dopp <48069535+Donkmemes@users.noreply.github.com>
2023-08-27 11:02:43 -0400

    fixed bugs and completed project :)

--- script.js
+++ script.js
@@ -1,80 +1,81 @@
-let cpu_score = 0;
 let player_score = 0;
-console.log(1);
+let cpu_score = 0;
+let result = "";
 
-game(cpu_score, player_score);
+game();
+
+function game(){
+    let playerChoice = prompt("Rock Paper or Scissors");
+    let computerChoice = getComputerChoice();
+
+    playRound(playerChoice, computerChoice);
+
+    if(player_score == 3){
+        result = "You Win!"
+        return;
+    }
+    else if(cpu_score == 3){
+        result = "Computer Wins!"
+        return;
+    }
+    else{
+        game();
+    }
+}
+
+function playRound(playerChoice, computerChoice){
+    if(playerChoice == "Rock"){
+        if(computerChoice == "Paper"){
+            cpu_score+=1;
+            console.log("Computer Wins This Round!");
+        }
+        else if(computerChoice == "Scissors"){
+            player_score+=1;
+            console.log("You Wins This Round!");
+        }
+        else{
+            console.log("Tie!");
+        }
+    }
+    else if(playerChoice == "Paper"){
+        if(computerChoice == "Scissors"){
+            cpu_score+=1;
+            console.log("Computer Wins This Round!");
+        }
+        else if(computerChoice == "Rock"){
+            player_score+=1;
+            console.log("You Wins This Round!");
+        }
+        else{
+            console.log("Tie!");
+        }
+    }
+    else{
+        if(computerChoice == "Rock"){
+            cpu_score+=1;
+            console.log("Computer Wins This Round!");
+        }
+        else if(computerChoice == "Paper"){
+            player_score+=1;
+            console.log("You Wins This Round!");
+        }
+        else{
+            console.log("Tie!");
+        }
+    }
+}
 
 function getComputerChoice(){
-    const rand = Math.floor(Math.random() * 3) + 1
+    const rand = Math.floor(Math.random() * 3) + 1;
     if(rand == 1){
-        console.log("works");
         return "Rock";
     }
     else if(rand == 2){
-        console.log("works");
         return "Paper";
     }
     else{
-        console.log("works");
         return "Scissors";
     }
 }
 
-function playRound(playerSelection, computerSelection){ 
-    if(playerSelection == "Rock"){
-        if(computerSelection == "Rock"){
-            console.log("Tie!"); 
-        }
-        else if(computerSelection == "Paper"){
-            console.log("You Lose!");
-            cpu_score+=1; 
-        }
-        else{
-            console.log("You Win!");
-            player_score+=1; 
-        }
-    }
-    if(playerSelection == "Paper"){
-        if(computerSelection == "Paper"){
-            console.log("Tie!");
-        }
-        else if(computerSelection == "Scissors"){
-            console.log("You Lose!");
-            cpu_score+=1;
-        }
-        else{
-            console.log("You Win!");
-            player_score+=1;
-        }
-    }
-    if(playerSelection == "Scissors"){
-        if(computerSelection == "Scissors"){
-            console.log("Tie!");
-        }
-        else if(computerSelection == "Rock"){
-            console.log("You Lose!");
-            cpu_score+=1;
-        }
-        else{
-            console.log("You Win!");
-            player_score+=1;
-        }
-    }
-}
-
-function game(cpu_score, player_score){
-    let computerSelection = getComputerChoice();
-    let playerSelection = prompt();
-    playRound(computerSelection, playerSelection);
-    if(cpu_score == 3){
-        console.log("Computer Wins!");
-        return;
-    }
-    if(player_score == 3){
-        console.log("You Win!");
-        return;
-    }
-    else{
-        game(cpu_score, player_score);
-    }
-}
+console.log(result);
